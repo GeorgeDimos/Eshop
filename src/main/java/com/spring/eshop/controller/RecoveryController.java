b95e83c521b3987a7fadfdb958034763d50c993b
GeorgeDimos <dimos@uth.gr>
2020-12-16 17:51:58 +0200

    Fixed password recovery validation. Moved fragments to their own folder.

--- src/main/java/com/spring/eshop/controller/RecoveryController.java
+++ src/main/java/com/spring/eshop/controller/RecoveryController.java
@@ -1,12 +1,17 @@
 package com.spring.eshop.controller;
 
+import com.spring.eshop.entity.User;
 import com.spring.eshop.exceptions.InvalidUserInfoException;
 import com.spring.eshop.service.interfaces.IUserConfirmationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @Controller
 @RequestMapping("/recover")
@@ -22,7 +27,7 @@ public class RecoveryController {
 	@GetMapping("/activationEmail")
 	public String getActivationEmail(Model model) {
 		model.addAttribute("title", "Resend Activation Email");
-		return "recover-password";
+		return "/recover-password";
 	}
 
 	@PostMapping("/activationEmail")
@@ -33,20 +38,20 @@ public class RecoveryController {
 
 		try {
 			userConfirmationService.resendActivationEmail(username, email);
-			redirectAttributes.addFlashAttribute("recovery",
+			redirectAttributes.addFlashAttribute("success",
 					"Check your email for the activation link");
 			return "redirect:/login";
 		} catch (InvalidUserInfoException ex) {
 			model.addAttribute("title", "Resend Activation Email");
 			model.addAttribute("error", ex.getMessage());
-			return "recover-password";
+			return "/recover-password";
 		}
 	}
 
 	@GetMapping("/password")
 	public String getPasswordRecovery(Model model) {
 		model.addAttribute("title", "Recover Password");
-		return "recover-password";
+		return "/recover-password";
 	}
 
 	@PostMapping("/password")
@@ -57,30 +62,36 @@ public class RecoveryController {
 
 		try {
 			userConfirmationService.sendPasswordRecoveryEmail(username, email);
-			redirectAttributes.addFlashAttribute("recovery",
+			redirectAttributes.addFlashAttribute("success",
 					"Check your email for password recovery instructions");
 			return "redirect:/login";
 		} catch (InvalidUserInfoException ex) {
 			model.addAttribute("title", "Recover Password");
 			model.addAttribute("error", ex.getMessage());
-			return "recover-password";
+			return "/recover-password";
 		}
 	}
 
 	@GetMapping("/{token}")
-	public String getPasswordInputPage(@PathVariable String token) {
+	public String getPasswordInputPage(@PathVariable String token, Model model) {
 		if (!userConfirmationService.isTokenValid(token)) {
 			return "redirect:/error";
 		}
-		return "new-password";
+		model.addAttribute("user", new User());
+		return "/new-password";
 	}
 
 	@PostMapping("/{token}")
 	public String changePassword(@PathVariable("token") String token,
-								 @RequestParam("password") String password,
+								 @Valid @ModelAttribute("user") User user,
+								 BindingResult bindingResultUser,
 								 RedirectAttributes redirectAttributes) {
-		userConfirmationService.confirmPasswordChange(token, password);
-		redirectAttributes.addFlashAttribute("recovery", "Password successfully changed");
+
+		if(bindingResultUser.hasFieldErrors("password")){
+			return "/new-password";
+		}
+		userConfirmationService.confirmPasswordChange(token, user.getPassword());
+		redirectAttributes.addFlashAttribute("success", "Password successfully changed");
 		return "redirect:/login";
 	}
 }
